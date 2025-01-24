2e89d25d8f72fc80a6dae79d553feccbf425e2c6
Kissdani05 <kissdani05@gmail.com>
2025-01-24 10:45:05 +0100

    Add project files.

---
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -0,0 +1,39 @@
+﻿namespace ShoppingCartProject.Models
+{
+    public class ShoppingCart
+    {
+        private List<Product> _products;
+
+        public ShoppingCart()
+        {
+            _products = new List<Product>();
+        }
+
+        public int ProductCount => _products.Count;
+
+        public void AddProduct(string name, double price)
+        {
+            _products.Add(new Product(name, price));
+        }
+
+        public void RemoveProduct(string name)
+        {
+            var product = _products.FirstOrDefault(p => p.Name == name);
+            if (product == null)
+            {
+                throw new InvalidOperationException($"The product '{name}' is not in the cart.");
+            }
+            _products.Remove(product);
+        }
+
+        public double GetTotalPrice()
+        {
+            return _products.Sum(p => p.Price);
+        }
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(_products);
+        }
+    }
+}
